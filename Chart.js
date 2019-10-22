2679180b4d89d8dfcd91dc0eb74c4da5e5d345d6
Maurice <mauricedroll@gmail.com>
2019-10-22 14:11:45 +0200

    Added statistic page

--- Chart.js
+++ Chart.js
@@ -1,54 +1,74 @@
-var ctx = document.getElementById('myChart').getContext('2d');
-var chart = new Chart(ctx, {
-    // The type of chart we want to create
-    type: 'line',
+function loadChart() {
 
-    // The data for our dataset
-    data: {
-        labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
-        datasets: [{
-            label: 'Koffein in mg (empfohlenene Menge)',
-            backgroundColor: 'rgb(255, 0, 0)',
-            borderColor: 'rgb(255, 99, 132)',
-            data:Array.apply(null, new Array(12)).map(Number.prototype.valueOf, 400),
-            fill:false,
-        }, 
-        {
-            label: 'zug. Koffein',
-            backgroundColor: 'rgb(0,255,0)',
-            borderColor: 'rgb(0,255,0)',
-            data: [100,130,200,300,400,500,100],
-            fill:false,
+    let ctx = document.getElementById('myChart').getContext('2d');
+    let chart = new Chart(ctx, {
 
+        type: 'bar',
+
+        // The data for our dataset
+        data: {
+            labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+            datasets: [{
+                label: 'Koffein in mg (empfohlenene Menge)',
+                backgroundColor: 'blue',
+                borderColor: 'black',
+                data: Array.apply(null, new Array(12)).map(Number.prototype.valueOf, 400),
+                fill: false,
+                type: 'line',
+            },
+            {
+                label: 'zug. Koffein durch Latte Macchiato',
+                backgroundColor: 'rgb(0,255,0)',
+                borderColor: 'rgb(0,255,0)',
+                data: [100, 130, 200, 300, 400, 500, 100],
+                fill: false,
+
+            },
+            {
+                label: 'Koffeinmenge durch Kaffee',
+                data: [50,10,123,54,60]
+            }
+            ]
+        },
+
+
+        // Defintion der gestapelten Diagramme
+        options: {
+            scales:{
+                xAxes: [{ stacked: true }],
+                yAxes: [{ stacked: true }]
+              }
         }
-    ]
-    },
-
-
-    // Configuration options go here
-    options: {}
-});
-var sec_Chart= document.getElementById('secondChart').getContext('2d');
-var sec_chart = new Chart(secondChart, {
-    // The type of chart we want to create
-    type: 'line',
-
-    // The data for our dataset
-    data: {
-        labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
-        datasets: [ {
-            label: 'Zucker',
-            backgroundColor: 'rgb(0,0,255)',
-            borderColor: 'rgb(0,0,255)',
-            data:[0,100,200,300,450,100,350],
-            fill:false,
-            display: 'auto',
         }
-        
-        ]
-    },
+    );
+    let sec_Chart = document.getElementById('secondChart').getContext('2d');
+    let sec_chart = new Chart(secondChart, {
+        // Definition eines Säulendiagramms
+        type: 'bar',
+        // The data for our dataset
+        data: {
+            labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+            datasets: [{
+                label: 'Zucker',
+                backgroundColor: 'rgb(0,0,255)',
+                borderColor: 'rgb(0,0,255)',
+                data: [0, 100, 200, 300, 450, 100, 350],
+                fill: false,
+                display: 'auto',
+            }
+            ]
+        },
 
 
-    // Configuration options go here
-    options: {}
-});
+        // Configuration options go here
+        options: {}
+    })
+
+};
+
+
+function getCoffee(){
+    let koffein= 
+    alert()
+
+}
--- templates/statistik.js
+++ templates/statistik.js
@@ -1,4 +1,21 @@
 const statistik = '' +
-    '<p>hier html einfügen</p>' +
-    '<p>muss in den Klammern stehen und mit + abgegrenzen</p>' +
-    '<p>css bitte in die zur Komponente gehörende css datei</p>';
+    '<h2>Statistik</h2>' +
+// // Navbar
+//     '<nav class="navbar navbar-expand-lg navbar-dark sticky-top">'+
+//     '<div class="navcontainer">'+
+//         '<img src="assets/coffee.svg" class="navicon">'+
+//         '<a href="#" onclick="onNavigate("/koffein"); return false;" class="nav-item">Koffein</a>'+
+//         '<a href="#" onclick="onNavigate("/zucker"); return false;" class="nav-item">Zucker</a>'+
+//         '<a href="#" onclick="onNavigate("/kalorien"); return false;" class="nav-item">Kalorien</a>'+
+//         '<a href="#" onclick="onNavigate("/kosten"); return false;" class="nav-item">Kosten</a>'+
+//     '</div>'+
+//     '</nav>'+
+// //Navbar_ende
+    
+    '<h2> &Uuml;bersicht über Koffeinkonsum </h2>'+
+    '<canvas id="myChart" width="400" height="400" > 5 </canvas>'+
+    '<br>'+
+    '<h2> &Uuml;bersicht über Zuckerkonsum </h2>'+
+    '<canvas id="secondChart"> </canvas>'+
+    '<style onload= "loadChart()"> </style>'
+    ;
